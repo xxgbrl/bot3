9a067536d9af9737fb8302bda023c95e79686e7d
xxgbrl <zupper.id@gmail.com>
2023-02-20 20:41:47 +0700

    xxgbrl

---
+++ plugins/owner-website.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+  let caption = `
+*「 Owner Website 」*
+
+Main Pages   -> ryzn.my.id
+Blogspot    -> www.win-tweak.my.id
+Uptime      -> uptime.ryzn.my.id
+
+----------------------
+Prosekai Sticker Maker -> sekai.ryzn.my.id
+----------------------
+
+Revanced    -> ryzn.my.id/revanced
+Spotify Mod -> ryzn.my.id/spotify
+Mod Manager -> ryzn.my.id/mods
+`.trim()
+  m.reply(caption)
+}
+handler.help = ['website']
+handler.tags = ['main']
+handler.command = /^(website)$/i
+handler.limit = false
+
+export default handler
